bb39f9553d2dd67150ec5b5a2e3871e974021c3a
Oksana Cherniavskaia <oksana.cherniavskaia@dbeaver.com>
2024-12-15 01:42:33 +0300

    HW-01: Fix resource loading.

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,17 +1,18 @@
 package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -25,31 +26,20 @@ public class CsvQuestionDao implements QuestionDao {
         // https://mkyong.com/java/java-read-a-file-from-resources-folder
         ClassLoader classLoader = getClass().getClassLoader();
         String fileName = fileNameProvider.getTestFileName();
-        URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
+
+        try (InputStream is = classLoader.getResourceAsStream(fileNameProvider.getTestFileName())) {
+
+            List<QuestionDto> questionDtos = new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(is))
+                            .withType(QuestionDto.class)
+                            .withSeparator(';')
+                            .withSkipLines(1)
+                            .build()
+                            .parse();
+
+            return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException exception) {
             throw new QuestionReadException(
-                    String.format("Unable to load resource:  %s", fileName));
-        } else {
-            try {
-                File file = new File(resource.toURI());
-                List<QuestionDto> questionDtos = readQuestionsFromCsv(file);
-
-                return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
-            } catch (URISyntaxException e) {
-                throw new QuestionReadException(
-                        String.format("Incorrect uri syntax for %s", fileName), e);
-            } catch (FileNotFoundException e) {
-                throw new QuestionReadException(String.format("File not found %s", fileName), e);
-            }
+                    String.format("Error on reading resource %s", fileName), exception);
         }
-    }
-
-    private static List<QuestionDto> readQuestionsFromCsv(File file) throws FileNotFoundException {
-        return new CsvToBeanBuilder<QuestionDto>(new FileReader(file))
-                .withType(QuestionDto.class)
-                .withSeparator(';')
-                .withSkipLines(1)
-                .build()
-                .parse();
     }
 }
