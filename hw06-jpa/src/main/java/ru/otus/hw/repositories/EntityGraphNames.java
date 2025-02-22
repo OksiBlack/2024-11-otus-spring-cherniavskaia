748cec1dab3e504a9f13ce24bb9f3b163db119b0
Oksana Cherniavskaia <oksana.cherniavskaia@dbeaver.com>
2025-02-22 13:06:38 +0300

    HW-06: Correct graphs.

--- hw06-jpa/src/main/java/ru/otus/hw/repositories/EntityGraphNames.java
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/EntityGraphNames.java
@@ -1,8 +1,6 @@
 package ru.otus.hw.repositories;
 
 public class EntityGraphNames {
-    public static final String BOOK_ENTITY_GRAPH = "book-entity-graph";
-
     public static final String COMMENT_BOOK_GRAPH = "comment-book-graph";
 
     public static final String FULL_BOOK_ENTITY_GRAPH = "book-entity-graph";
