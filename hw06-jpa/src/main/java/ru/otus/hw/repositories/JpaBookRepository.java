faf514fb8091bd5818bffe941d1a53e1667037f2
Oksana Cherniavskaia <oksana.cherniavskaia@dbeaver.com>
2025-02-22 13:06:38 +0300

    HW-06: Correct graphs.

--- hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaBookRepository.java
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaBookRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static ru.otus.hw.repositories.EntityGraphNames.BOOK_ENTITY_GRAPH;
+import static ru.otus.hw.repositories.EntityGraphNames.FULL_BOOK_ENTITY_GRAPH;
 
 @Repository
 public class JpaBookRepository implements BookRepository {
@@ -23,7 +23,7 @@ public class JpaBookRepository implements BookRepository {
     @Override
     public Optional<Book> findById(Long id) {
 
-        EntityGraph<?> entityGraph = em.getEntityGraph(BOOK_ENTITY_GRAPH);
+        EntityGraph<?> entityGraph = em.getEntityGraph(FULL_BOOK_ENTITY_GRAPH);
         Map<String, Object> properties = new HashMap<>();
         properties.put("jakarta.persistence.fetchgraph", entityGraph);
 
@@ -32,7 +32,7 @@ public class JpaBookRepository implements BookRepository {
 
     @Override
     public List<Book> findAll() {
-        EntityGraph<?> entityGraph = em.getEntityGraph(BOOK_ENTITY_GRAPH);
+        EntityGraph<?> entityGraph = em.getEntityGraph(FULL_BOOK_ENTITY_GRAPH);
 
         TypedQuery<Book> query = em.createQuery("select b from Book b", Book.class);
         query.setHint("jakarta.persistence.fetchgraph", entityGraph);
